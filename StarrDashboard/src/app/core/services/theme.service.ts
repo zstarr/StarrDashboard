3d3016873571bf3e5d2c2ee955a9c42d609811af
Zach Starr <zach@starr.biz>
2020-08-23 14:25:36 -0500

    Added light-theme for overlays containers

--- StarrDashboard/src/app/core/services/theme.service.ts
+++ StarrDashboard/src/app/core/services/theme.service.ts
@@ -1,24 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Subject, ReplaySubject, BehaviorSubject } from 'rxjs';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Injectable()
 export class ThemeService {
   private _darkTheme: Subject<boolean> = null;
   isDarkTheme = null;
   localStorageKey: string = 'DarkTheme';
+  overlayContainerState: OverlayContainer;
 
-  constructor() {
+  constructor(overlayContainer: OverlayContainer) {    
+    this.overlayContainerState = overlayContainer;
     if (localStorage.getItem(this.localStorageKey) === null) {
-      localStorage.setItem(this.localStorageKey, JSON.stringify(true));
+      localStorage.setItem(this.localStorageKey, JSON.stringify(true));      
     }
     this._darkTheme = new BehaviorSubject<boolean>(
       JSON.parse(localStorage.getItem(this.localStorageKey))
     );
-    this.isDarkTheme = this._darkTheme.asObservable();
+    this.isDarkTheme = this._darkTheme.asObservable();    
+    this.setOverlayClass(JSON.parse(localStorage.getItem(this.localStorageKey)));
   }
 
   setDarkTheme(isDarkTheme: boolean) {
     this._darkTheme.next(isDarkTheme);
+    this.setOverlayClass(isDarkTheme);    
     localStorage.setItem(this.localStorageKey, JSON.stringify(isDarkTheme));
+  }
+
+  setOverlayClass(isDarkTheme: boolean) {
+    if (isDarkTheme) {
+      this.overlayContainerState.getContainerElement().classList.remove('light-theme')
+      this.overlayContainerState.getContainerElement().classList.add('dark-theme');
+      }
+    else {
+      this.overlayContainerState.getContainerElement().classList.remove('dark-theme')
+      this.overlayContainerState.getContainerElement().classList.add('light-theme');
+      }
   }
 }
